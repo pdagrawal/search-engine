062afef28c79caa8195aa75c107c7266d9814a48
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-04-20 21:05:25 -0400

    retrieval working and printing the results in terminal

--- retrieve.py
+++ retrieve.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import time
 from indexing import load_stop_words
 from prettytable import PrettyTable
@@ -8,13 +9,13 @@ from prettytable import PrettyTable
 def main(query_words, query_wts = None):
     query_words = [word.lower() for word in query_words.split(',') if len(word) > 1 and (word not in load_stop_words())]
     postings_list = get_postings_for_query(query_words)
-    print(postings_list)
     query_weights = calculate_query_weights(query_words, query_wts)
-    print(query_weights)
-    # numerator_values = calculate_numerators(query_words, postings_list, query_weights, documents)
-    # query_denominator = square_root_of_sum_of_squares(query_weights.values())
-    # similarity_scores = calculate_similarity(query_words, postings_list, numerator_values,
-    #                                         query_denominator, documents)
+    documents = {key: 0 for key in sorted(os.listdir('files/'))}
+    numerator_values = calculate_numerators(query_words, postings_list, query_weights, documents)
+    query_denominator = square_root_of_sum_of_squares(query_weights.values())
+    similarity_scores = calculate_similarity(query_words, postings_list, numerator_values,
+                                            query_denominator, documents)
+    print(similarity_scores)
     # search_result = PrettyTable()
     # search_result.field_names = ["Rank", "Document ID", "Similarity Score"]
     # for index, name in enumerate(list(similarity_scores.keys())[:10]):
@@ -41,20 +42,8 @@ def get_postings_for_query(query_words):
         starting_index = int(dictionaries[word_index+2])
         for i in range(0,int(dictionaries[word_index+1])):
             posting = postings[starting_index-1+i]
-            postings_list[word][posting.split(',')[0]] = posting.split(',')[1]
+            postings_list[word][posting.split(',')[0]] = float(posting.split(',')[1])
     return postings_list
-
-# Read postings file to get term weights
-def get_postings_for_word(word):
-    postings_list = {}
-    with open('output_files/postings_file.txt', 'r') as postings:
-        for line in postings:
-            if line.split()[0] == word:
-                for doc in line.split()[1:]:
-                    postings_list[doc] = float(doc.split(':')[1])
-                break
-    return postings_list
-
 
 # Calculate query weights using their frequency in query
 def calculate_query_weights(words, weights):
@@ -64,7 +53,7 @@ def calculate_query_weights(words, weights):
             query_wts[word] = words.count(word)/len(words)
     else:
         for word in list(set(words)):
-            query_wts[word] = weights.split(',')[list(set(words)).index(word)]
+            query_wts[word] = float(weights.split(',')[list(set(words)).index(word)])
     return query_wts
 
 # Calculate numerators for each document
@@ -72,7 +61,7 @@ def calculate_numerators(query_words, postings_list, query_weights, numerators):
     for term in query_words:
         for doc in numerators.keys():
             if term in postings_list and doc in postings_list[term]:
-                numerators[doc] += postings_list[term][doc] * float(query_weights[term])
+                numerators[doc] += postings_list[term][doc] * query_weights[term]
     return numerators
 
 def square_root_of_sum_of_squares(numbers):
@@ -100,4 +89,4 @@ if __name__ == "__main__":
     else:
         main(sys.argv[1])
     end = time.process_time_ns()
-    print("Total elapsed time:", (end - start)/1000000)
+    print(f"Total elapsed time: {(end - start)/1000000} ms")
